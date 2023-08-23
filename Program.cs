a21c706b714e93e1c40d62115e9460ea72ead266
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 23:32:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Iveskite temperatura: ");
+int temp = Convert.ToInt32(Console.ReadLine());
+if (temp < 0)
+{
+    Console.WriteLine("Salta");
+}
+else if (temp >=0 && temp <=20)
+{
+   Console.WriteLine("Vesu");
+}
+else if (temp > 20)
+{
+    Console.WriteLine("Karsta");
+}
